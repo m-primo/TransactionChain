60c1a58ab942bfd4b7c87053eda7298bef43d45a
Primo <44984918+m-primo@users.noreply.github.com>
2020-04-23 21:05:40 +0200

    v0.4

--- main.php
+++ main.php
@@ -1,29 +1,6 @@
 <?php
-//error_reporting(0);
-declare(strict_types=1);
-define('eol', PHP_EOL);
-define('DS', DIRECTORY_SEPARATOR);
-define('DIR_SRC', 'src'.DS);
-define('DIR_APP', 'app'.DS);
-define('DIR_DB', 'db'.DS);
-define("DATE_FORMAT", "M d, Y H:i:s");
-define('TRANSACTIONS_DB_FILE', DIR_DB.'chain.db.json');
-define('ROUND_PREC', 2);
-define('APP_TYPE', 'cli');
-// ---------------------------
-spl_autoload_register(function($name) {
-    $fName = DIR_SRC.$name.'.class.php';
-    if(file_exists($fName)) require_once $fName;
-});
-//-------------------------
-$transactions = [];
-if(!file_exists(TRANSACTIONS_DB_FILE)) {
-    new Transaction(null, null, "Genesis Transaction");
-    Chain::save();
-}
-$transactions = json_decode(file_get_contents(TRANSACTIONS_DB_FILE), true);
+define('fds', '');
+require_once 'inc.php';
 // ---------------------
-$app_types = ['cli'];
-// ---------------------
-if(in_array(APP_TYPE, $app_types)) require_once DIR_APP.'app.'.strtolower(APP_TYPE).'.php';
+require_once DIR_APP.'app.cli.php';
 ?>
